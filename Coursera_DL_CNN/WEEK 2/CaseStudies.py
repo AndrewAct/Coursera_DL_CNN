eb200469fc114326a73d9358887d8b5f5043f66f
Andrew Chen <43218650+AndrewAct@users.noreply.github.com>
2020-02-09 08:06:12 -0500

    Add files via upload

---
+++ Coursera_DL_CNN/WEEK 2/CaseStudies.py
@@ -0,0 +1,66 @@
+##########  Module 1  ##########
+######### Case Studies #########
+############ 2/7/2020  #########
+
+# Why Look at Case Studies? #
+# Classci networks 
+# LeNet - 5
+
+# AlexNet
+# Similar model but much bigger 
+# ReLLu 
+# Training multiple GPUs
+
+# VGG 
+# 16 layers
+
+# ResNet 
+
+# Inception 
+
+ 
+# Classic Networks # 
+
+
+# Residual Networks #
+# "short cut"/ skip connection 
+
+# Why ResNetwork ? # 
+# It skip some steps, but the feature extracting is not hurt 
+
+
+# Networks in Networks and 1 X 1Convolution # 
+# Shrink the number of convolution 
+
+# 2/8/2020 # 
+# The problem of computational cost 
+# Bottleneck layer 
+
+# Inception Module #
+
+##########  Module 2  ##########
+## Practical Advices for Using ConvNets #
+
+# Use Open-Source Implementation #
+# GitHub 
+
+# Transfer Learning #
+
+# Data Augumentation #
+# Mirroring method 
+
+# Color shifting 
+
+# Implement distortions during training 
+
+# State of Computer Vision #
+# Two sources of knowledge
+# - laabeled data 
+# - Hand engoineered features 
+
+# Tips on doing well on Benchmark/Winning competitions
+# Ensembling
+# - Train severalnetworks independently 
+# - Multiple-crop at test time
+# - Use open source code 
+# - Use pertained models 
