14cbd346cd29a9d9d7dd1aac653e066d78715817
Andrew Chen <43218650+AndrewAct@users.noreply.github.com>
2020-02-16 23:27:22 -0500

    Add files via upload

---
+++ Coursera_DL_CNN/WEEK 3/Lecture.py
@@ -0,0 +1,24 @@
+##########  Module 1  ##########
+####  Detection Algorithm  ####
+
+# 2/9/2020 #
+# Objection Localization #
+# Classification with Localization #
+# Need to output b[x], b[y], b[h], b[w], class label (1-4)
+
+# Landmark Detection # 
+
+# Objection Detection #
+# Sliding window detection 
+# Compurtional loss 
+
+
+# Convolutional Implementation of Sliding Windows #
+# This method may not be very accurate (?)
+
+# Bounding Box Predictions # 
+# YOLO: You only look once 
+
+# Labels for training:
+# For each grid cell: 
+# Convolutional mutation 
