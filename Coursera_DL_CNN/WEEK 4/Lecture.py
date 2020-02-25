270182360d074a71a42fdb7ab0f9b06f23d4ac06
Andrew Chen <43218650+AndrewAct@users.noreply.github.com>
2020-02-24 23:30:41 -0500

    Add files via upload

---
+++ Coursera_DL_CNN/WEEK 4/Lecture.py
@@ -0,0 +1,13 @@
+##########  Module 1  ##########
+####### Face Recognition ######
+
+## What is face recognition ?
+# Face verification 
+# Get the ID of face 
+
+# Face recognition 
+# Have datasets of faces
+
+# Similarity function 
+
+# Triple Loss # 
